3c34dd8271f4da9edb091eebdb89fcf11f722ac4
StarPandaBeg <kshmurakov@mail.ru>
2024-07-09 19:29:00 +0300

    fix: throw only internal redirect error

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,15 +1,14 @@
 import { QueryClientConfig } from "@tanstack/react-query";
-import { AxiosError } from "axios";
 
 export const clientConfig = {
   defaultOptions: {
     queries: {
       staleTime: 60 * 1000,
       throwOnError(error, query) {
-        if (error instanceof AxiosError) return false;
-
-        // TODO: Return true only for NextRedirect
-        return true;
+        if (error instanceof Error) {
+          if (error.message === "NEXT_REDIRECT") return true;
+        }
+        return false;
       },
     },
   },
