9fe1a74cacff2a567d5fa6fb1961e726faa6b9ca
StarPandaBeg <kshmurakov@mail.ru>
2024-07-09 15:55:08 +0300

    feat: install Prettier
    - install plugin to organize imports
    - install plugin to order Tailwind classes

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  trailingComma: "es5",
+  semi: true,
+  tabWidth: 2,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  endOfLine: "auto",
+  plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-organize-imports"],
+};
