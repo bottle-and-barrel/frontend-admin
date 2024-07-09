360d2e00ec9eba0b63287284f86e6460931ccb0d
StarPandaBeg <kshmurakov@mail.ru>
2024-07-09 17:32:29 +0300

    feat: add auth api

---
+++ src/api/auth.ts
@@ -0,0 +1,53 @@
+import client from "@/lib/axios";
+import { User } from "./user";
+
+const ENDPOINT = "/auth";
+
+export interface AuthCredentials {
+  email: string;
+  password: string;
+}
+
+export interface AuthRefreshCredentials {
+  refresh_token: AuthToken | string;
+}
+
+export interface AuthToken {
+  token: string;
+  expires: number;
+}
+
+export interface AuthResult {
+  user_id: number;
+  access_token: AuthToken;
+  refresh_token: AuthToken;
+}
+
+export async function auth(credentials: AuthCredentials) {
+  const headers = {};
+  const { data } = await client.post<AuthResult>(ENDPOINT, credentials, {
+    headers,
+  });
+  return data;
+}
+
+export async function refresh(credentials: AuthRefreshCredentials) {
+  const possibleToken = credentials.refresh_token;
+  const token =
+    typeof possibleToken === "string" ? possibleToken : possibleToken.token;
+
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  };
+  const { data } = await client.post<AuthResult>(
+    `${ENDPOINT}/refresh`,
+    {},
+    { headers }
+  );
+  return data;
+}
+
+export async function me() {
+  const { data } = await client.get<User>(`${ENDPOINT}/me`);
+  return data;
+}
