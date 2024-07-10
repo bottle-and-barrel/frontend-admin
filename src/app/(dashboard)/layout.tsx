b5b7b389f4efde821b24e4f4d9a19cd0809ad88d
StarPandaBeg <kshmurakov@mail.ru>
2024-07-10 19:25:52 +0300

    fix: make page-side-navigation fixed

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -9,7 +9,9 @@ export default function DashboardLayout({
 }>) {
   return (
     <main className="flex h-full flex-col sm:grid sm:grid-cols-[56px_1fr] sm:grid-rows-[auto_1fr_auto]">
-      <PageSideNavigation className="col-start-1 row-span-3 hidden sm:flex" />
+      <div className="relative col-start-1 row-span-3 hidden sm:block">
+        <PageSideNavigation className="h-full fixed left-0 top-0" />
+      </div>
       <PageHeader className="col-start-2" />
       <div className="col-start-2 row-start-2 p-4">{children}</div>
       <PageFooter className="col-start-2 row-start-3 mt-auto h-auto" />
